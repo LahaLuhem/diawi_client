f8de09493e7f8bf024a9c308e0265c32b7aef507
M-Ahal <m.ahal@didata.org>
2025-07-27 14:33:05 +0200

    use json_serializable library + copyWith

--- Library/dart_openai-generator-cli.sh
+++ Library/dart_openai-generator-cli.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit 1
+set -e
 
 # Preamble
 client_library_name=$(basename "$(dirname "$PWD")")
@@ -8,6 +9,14 @@ read -r -n 1 -p "The client name will be '$client_library_name'. Press any key t
 # Ensure dependencies
 export PATH="$PATH":"$HOME/.puro/shared/pub_cache/bin"
 dart pub global activate openapi_generator_cli
+# Puro proxying
+if command -v puro &> /dev/null; then
+  # shellcheck disable=SC2317
+  flutter() { puro flutter "$@"; };
+  dart() { puro dart "$@"; };
+  export -f flutter;
+  export -f dart;
+fi
 
 # Declarations
 api_version_tag="3"
@@ -21,7 +30,10 @@ rm -f ./*.yaml
 rm -f README.md
 rm -f pubspec.lock
 
-# Execute generators
+
+
+
+# Execute openapi-generator generator
 cd ..
 openapi-generator generate                                                        \
         -i https://petstore3.swagger.io/api/v${api_version_tag}/openapi.json      \
@@ -33,15 +45,50 @@ openapi-generator generate                                                      
         --additional-properties pubAuthor="$(git config user.name)"               \
         --additional-properties pubAuthorEmail="$(git config user.email)"         \
         --additional-properties pubHomepage="https://onepub.dev/api/ylznnoflyg"   \
-        --additional-properties pubPublishTo="https://onepub.dev/api/ylznnoflyg"
+        --additional-properties pubPublishTo="https://onepub.dev/api/ylznnoflyg"  \
+        --additional-properties pubDescription="Petstore OpenAPI API client"      \
+        --additional-properties serializationLibrary="json_serializable"
+# Cleanup
+dart pub global deactivate openapi_generator_cli
 
+
+
+
+# Add copyWith annotations
+dart pub add copy_with_extension
+dart pub add --dev copy_with_extension_gen
+
+# Annotate every class with `CopyWith`
+find lib/src/model -type f -name '*.dart' | while read -r file; do
+# Insert import if missing
+grep -q "copy_with_extension" "$file" || sed -i '' "/package:json_annotation/a\\
+import 'package:copy_with_extension/copy_with_extension.dart';
+" "$file"
+
+# Insert @CopyWith() if missing
+grep -q "@CopyWith" "$file" || sed -i '' "/@JsonSerializable/i\\
+@CopyWith()
+" "$file"
+done
+
+
+
+
+# Pregen fixes
+read -r -n 1 -p "Please ensure that the Dart SDK (min) constraints in pubspec.yaml support null-aware-elements (3.8.1). Press ENTER to confirm and proceed. Press Ctrl+C to stop now."$'\n'
+
+
+
+
+# Run build_runner
 dart run build_runner build --delete-conflicting-outputs
+
+
+
 
 # Fixes
 dart fix --apply && dart format .
 
-# Cleanup
-dart pub global deactivate openapi_generator_cli
-
+# Postamble
 read -r -n 1 -p "CLEANUP DONE!. Press any key to exit..."
 exit 0
